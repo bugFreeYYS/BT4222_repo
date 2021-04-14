d956a8f26a0e2fb77dd330df12530a93788e9e72
SparklinZ <bolin.zhu123@gmail.com>
2021-04-14 17:49:00 +0800

    updated with train, validation, test

--- custom_utils/update_google_sheet.py
+++ custom_utils/update_google_sheet.py
@@ -5,11 +5,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('../data/client_secret.json', scope)
-client = gspread.authorize(credentials)
+try:
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('../data/client_secret.json', scope)
+    client = gspread.authorize(credentials)
 
-spreadsheet = client.open('cooked_complete_dataset')
+    spreadsheet = client.open('cooked_complete_dataset')
 
-with open('../data/cooked_data/cooked_complete_dataset.csv', 'r') as file_obj:
-    content = file_obj.read()
-    client.import_csv(spreadsheet.id, data=content) 
+    with open('../data/cooked_data/cooked_complete_dataset.csv', 'r') as file_obj:
+        content = file_obj.read()
+        client.import_csv(spreadsheet.id, data=content) 
+except:
+    pass
--- data/config.py
+++ data/config.py
@@ -3,7 +3,7 @@ config = {
     "cooked_dir": './cooked_data/',
     "raw_coindesk_file_name": "raw_coinDesk_data.csv",
     "raw_yahoo_finance_file_name": "raw_yahoo_finance_data.csv",
-    "cooked_data_file_name": "cooked_complete_dataset_local.csv",
+    "cooked_data_file_name": "cooked_complete_dataset_growing.csv",
 }
 
 
